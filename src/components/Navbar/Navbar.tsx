5c0e0cd2e883183224407d8f931a184592ff4183
Abdullah Al Maruf <al.maruf9921@gmail.com>
2025-09-02 23:17:30 +0600

    push

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="flex items-center justify-between w-full max-w-4xl px-4 py-2 mx-auto">
+      <div className="flex items-center p-4">
+        <a
+          href="/"
+          className="text-xl font-bold text-white hover:text-neutral-300"
+        >
+          Almas
+        </a>
+      </div>
+      <div className="hidden md:flex space-x-4 p-4">
+        <a
+          href="/about"
+          className="text-white text-lg font-medium hover:border-b-2 hover:border-purple-500 hover:text-neutral-300"
+        >
+          About
+        </a>
+        <a
+          href="/contact"
+          className="text-white text-lg font-medium hover:border-b-2 hover:border-purple-500 hover:text-neutral-300"
+        >
+          Contact
+        </a>
+        <button className="bg-purple-500 text-white text-lg font-medium px-6 py-2 rounded-md hover:bg-purple-600 transition duration-200">
+          Log In
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
